5a932086401615c3305e6110de5b1369ac1496fc
bekhzod tuychiev <toychiyevb@oks.uz>
2023-05-19 10:00:14 +0500

    fix: movies page

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -23,7 +23,7 @@ export default async function Movies() {
 						src={movie.vertical_poster.thumbnails.normal.src}
 						height={728}
 						width={1024}
-						className='select-none'
+						className='object-cover select-none aspect-[2/3]'
 						alt={movie.name}
 					/>
 					<p className='text-center'>{movie.name}</p>
